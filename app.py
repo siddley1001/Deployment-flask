75340f78a10b7e58dfd103897768c000841de370
Siddhartha Vanam <vanamsid@gmail.com>
2021-04-25 16:29:55 -0400

    refactored code for classification output

--- app.py
+++ app.py
@@ -3,35 +3,52 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('xgbc_model.pkl', 'rb'))
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+
+@app.route('/predict', methods=['POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    """For rendering results on HTML GUI"""
+    inputs = []
+    for x in request.form.values():
+        if x == 'Y':
+            x = 1
+        elif x == 'N':
+            x = 0
+        else:
+            x = int(x)
+        inputs.append(x)
 
-    output = round(prediction[0], 2)
-
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
-
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    '''
-    For direct API calls trought request
-    '''
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
+    final_inputs = [np.array(inputs)]
+    prediction = model.predict(final_inputs)
 
     output = prediction[0]
-    return jsonify(output)
+
+    if output == 1:
+        return render_template('index.html', prediction_text='Individual WILL have a stroke'
+                                                             '\n Note that the following model predicts strokes with '
+                                                             '96% '
+                                                             'accuracy')
+    else:
+        return render_template('index.html', prediction_text='Individual will NOT have a stroke'
+                                                             '\n Note that the following model predicts strokes with '
+                                                             '96% '
+                                                             'accuracy')
+
+# @app.route('/predict_api', methods=['POST'])
+# def predict_api():
+#     """For direct API calls through request"""
+#     data = request.get_json(force=True)
+#     prediction = model.predict([np.array(list(data.values()))])
+#
+#     output = prediction[0]
+#     return jsonify(output)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- model.py
+++ model.py
@@ -1,39 +1,75 @@
 # Importing the libraries
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import recall_score
+from xgboost import XGBClassifier
 
-dataset = pd.read_csv('hiring.csv')
+dataset = pd.read_csv('healthcare-dataset-stroke-data.csv')
+dataset.dropna(axis=0, inplace=True)
+dataset = dataset.drop('id', axis=1)
 
-dataset['experience'].fillna(0, inplace=True)
+'''Manipulate Features so they are machine readable'''
 
-dataset['test_score'].fillna(dataset['test_score'].mean(), inplace=True)
+# Gender
+dataset = dataset.loc[dataset.gender != 'Other']
+dataset['is_male'] = dataset['gender'].apply(lambda row: 1 if row == 'Male' else 0).astype(bool)
+dataset = dataset.drop('gender', axis=1)
 
-X = dataset.iloc[:, :3]
+# Hypertension
+dataset.hypertension = dataset['hypertension'].astype(bool)
 
-#Converting words to integer values
-def convert_to_int(word):
-    word_dict = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8,
-                'nine':9, 'ten':10, 'eleven':11, 'twelve':12, 'zero':0, 0: 0}
-    return word_dict[word]
+# Heart Disease
+dataset.heart_disease = dataset['heart_disease'].astype(bool)
 
-X['experience'] = X['experience'].apply(lambda x : convert_to_int(x))
+# Married
+dataset.ever_married = dataset['ever_married'].astype(bool)
 
-y = dataset.iloc[:, -1]
+# Work Type
+work_df = pd.get_dummies(dataset['work_type'], drop_first=True)
+dataset = dataset.drop('work_type', axis=1)
+dataset = pd.concat([dataset, work_df], axis=1)
 
-#Splitting Training and Test Set
-#Since we have a very small dataset, we will train our model with all availabe data.
+# Residence Type
+dataset['is_urban'] = dataset['Residence_type'].apply(lambda res: 1 if res == "Urban" else 0).astype(bool)
+dataset = dataset.drop('Residence_type', axis=1)
 
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
+# BMI: Generate a log feature and keep the original feature
+dataset['ln_bmi'] = np.log(dataset['bmi'])
 
-#Fitting model with trainig data
-regressor.fit(X, y)
+# Glucose: Generate a log feature and keep the original feature
+dataset['ln_glucose_lvl'] = np.log(dataset['avg_glucose_level'])
 
-# Saving model to disk
-pickle.dump(regressor, open('model.pkl','wb'))
+smokes_df = pd.get_dummies(dataset['smoking_status'], 'smoke', drop_first=True)
+dataset = dataset.drop('smoking_status', axis=1)
+dataset = pd.concat([dataset, smokes_df], axis=1)
 
-# Loading model to compare the results
-model = pickle.load(open('model.pkl','rb'))
-print(model.predict([[2, 9, 6]]))
+print(dataset.head())
+
+k, seed = 1, 42
+X = dataset.drop('stroke', axis=1)
+y = dataset.stroke
+
+# increases variety of training samples
+sm = SMOTE(sampling_strategy='auto', k_neighbors=k, random_state=seed)
+X_res, y_res = sm.fit_resample(X, y)
+
+# Splits data into train, validation, and test
+X_train, X_valid, y_train, y_valid = train_test_split(X_res, y_res, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.25)
+
+searched_params = {'subsample': 0.8, 'min_child_weight': 1, 'max_depth': 7, 'learning_rate': 0.25,
+                   'grow_policy': 'lossguide', 'gamma': 1.5, 'colsample_bytree': 0.6}
+xgbc = XGBClassifier(**searched_params)
+
+X_train = np.concatenate([X_train, X_valid], axis=0)
+y_train = np.concatenate([y_train, y_valid], axis=0)
+
+xgbc.fit(X_train, y_train)
+final_preds = xgbc.predict(X_test)
+
+print(recall_score(y_test, final_preds))
+
+pickle.dump(xgbc, open('xgbc_model.pkl', 'wb'))
